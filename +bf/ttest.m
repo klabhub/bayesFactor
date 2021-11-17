1d73857ad06f83023d53322926934d7772f71f8e
bartkrekelberg <bart@vision.rutgers.edu>
2021-11-17 06:40:20 -0500

    Fixes bug in ttest and ttest2
    Changed the interface for ttest and ttest2 to force specification of N and T and compute df internally.
    Interface for calls with actual data (X,Y) are unchanged.

--- +bf/ttest.m
+++ +bf/ttest.m
@@ -1,10 +1,10 @@
-function [bf10,pValue,CI,stats] = ttest(X,varargin)
+function [bf10,pValue] = ttest(X,varargin)
 %TTEST Bayes Factors for one-sample and paired t-tests.
-% function [bf10,p,CI,stats] = ttest(X,varargin)    - one sample
-% function [bf10,p,CI,stats] = ttest(X,M,varargin)   -one sample,non-zero mean
-% function [bf10,p,CI,stats] = ttest(X,Y,varargin)   -paired samples
+% [bf10,p] = ttest(X,varargin)    - one sample
+% [bf10,p] = ttest(X,M,varargin)   -one sample,non-zero mean
+% [bf10,p] = ttest(X,Y,varargin)   -paired samples
 %
-% function [bf10,p,CI,stats] = ttest('T',T,'df',df)   - calculate BF based
+% [bf10,p] = ttest('T',T,'N',10)   - calculate BF based
 % on regular ttest output
 %
 % INPUT 
@@ -15,21 +15,24 @@ function [bf10,pValue,CI,stats] = ttest(X,varargin)
 % Optional Parm/Value pairs:
 % tail - 'both','right', or 'left' for two or one-tailed tests [both]
 % scale - Scale of the Cauchy prior on the effect size  [sqrt(2)/2]
-% To calculated BF based on the outcome of a T-test, pass T and df as
-% parm/value pairs:
+%
+% To calculated BF based on the outcome of a T-test, pass T and the number of
+% samples as parm/value pairs:
 % T - The T-value resulting from a standard T-Test output 
-% df - the degrees of freedom of the T-test
+% N  - the number of samples used for the T-test
 %
 % OUTPUT
 % bf10 - The Bayes Factor for the hypothesis that the mean is different
 %           from zero. Using JZS priors. 
 % p - p value of the frequentist hypothesis test
 % CI    - Confidence interval for the true mean of X
-% stats - Structure with .tstat, .df,
 %
 % Based on: Rouder et al. J. Math. Psych. 2012
 % 
 % BK - Nov 2018
+% Nov 21 - changed interface to avoid confusion about df and N. Force
+% specifying T and N. Compute df internally.
+
 
 if isnumeric(X)
     if mod(numel(varargin),2)==0
@@ -46,32 +49,41 @@ if isnumeric(X)
         Y  =varargin{1};
     end
 else
-    %Neither X nor Y specified (must be a call with 'stats' specified
+    %Neither X nor Y specified (must be a call with 'T' and 'N' specified
     parms = cat(2,X,varargin);
     X=[];Y=[];
 end
+
 p=inputParser;
 p.addParameter('tail','both',@(x) (ischar(x)&& ismember(upper(x),{'BOTH','RIGHT','LEFT'})));
 p.addParameter('scale',sqrt(2)/2);
 p.addParameter('T',[],@isnumeric);
-p.addParameter('df',[],@isnumeric);
+p.addParameter('N',[],@isnumeric);
 p.parse(parms{:});
 
 tail = p.Results.tail;
 
 if isempty(p.Results.T)
     % Calculate frequentist from the X and Y data
-    [~,pValue,CI,stats] = ttest(X,Y,'tail',tail);
+    [~,pValue,~,stats] = ttest(X,Y,'tail',tail);
     T = stats.tstat;
     df = stats.df;
     N = numel(X);
+elseif isempty(p.Results.N)
+    error('N must be specified when calling bf.ttest with a T');  
 else
     % User specified outcome of frequentist test (the builtin ttest);
-    % Calculate BF from T and df.
-    
+    % Calculate BF from T and df.    
     T = p.Results.T;
-    df = p.Results.df;
-    
+    N = p.Results.N;
+    if numel(N)==2
+        % Call from a 2-sample T-tets (see bf.ttest2)
+        % Adjust df and N
+        df = sum(N)-2;
+        N = prod(N)/sum(N);
+    else
+        df = N-1;
+    end
     pValue = tcdf(T,df,'upper'); % Right tailed
         switch upper(tail)
             case 'BOTH'
@@ -80,22 +92,15 @@ else
                 pValue   =1-pValue ;
             case 'RIGHT'
                 % Ok as is
-        end
-    
-    N = p.Results.df+1;     
-    CI = [NaN NaN];
-    % Create stats struct to return the same output.
-    stats.tstat = T;
-    stats.df = df;
-    stats.sd = NaN;
-       
+        end        
 end
 
 % Use the formula from Rouder et al.
 % This is the formula in that paper; it does not use the
 % scale numerator = (1+T.^2/(N-1)).^(-N/2);
 % fun  = @(g) ( ((1+N.*g).^-0.5) .* (1+T.^2./((1+N.*g).*(N-1))).^(-N/2) .* (2*pi).^(-1/2) .* g.^(-3/2).*exp(-1./(2*g))  );
-% Here we use the scale  (Checked against Morey's R package )
+% Here we use the scale  (Checked against Morey's R package and
+% http://pcl.missouri.edu/bayesfactor)
 r = p.Results.scale;
 numerator = (1+T.^2/df).^(-(df+1)/2);
 fun  = @(g) ( ((1+N.*g.*r.^2).^-0.5) .* (1+T.^2./((1+N.*g.*r.^2).*df)).^(-(df+1)/2) .* (2*pi).^(-1/2) .* g.^(-3/2).*exp(-1./(2*g))  );
